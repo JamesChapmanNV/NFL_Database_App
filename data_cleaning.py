4f52a9d9b8002396ad085cc8ec084170994b879a
cazumbaugh3 <czumbaugh@platinumdeveloping.net>
2024-03-20 08:09:25 -0500

    Added methods to extract the numerical weight, convert displayHeight top height in inches, and clean the date of birth string.

--- data_cleaning.py
+++ data_cleaning.py
@@ -30,7 +30,6 @@ df_team_games.to_csv('data/decomposed_data/team_games.csv', index=False)
 
 df_games_decomposed = df_games[FINAL_GAMES_COLUMNS]
 df_games_decomposed.to_csv('data/decomposed_data/decomposed_games.csv', index=False)
-df_games_decomposed['displayWeight'].apply(lambda x: print(re.match(r'[0-9]+').group(0)))
 #%%
 
 """
@@ -45,11 +44,50 @@ def extract_number(s: str) -> str:
     except:
         return 'NULL'
 
+
+def get_height_inches(height_str: str) -> int:
+    # pattern of display height in [0-9]'[0-9]+" where the first
+    # value is height in feet and the second is height in inches.
+    try:
+        matches = re.findall(r'[0-9]+', height_str)
+        feet = matches[0]
+        inches = matches[1]
+        return int(feet) * 12 + int(inches)
+    except:
+        return pd.NA
+
+def clean_date(date_string: str) -> str:
+    # The date is formatted in one of several ways:
+    # %M-%d-%Y
+    # %M-%d-%y
+    # %m-%d-%Y
+    # %m-%d-%y
+    try:
+        date_values = re.findall(r'[0-9]+', date_string)
+        day = date_values[0]
+        month = date_values[1]
+        year = date_values[2]
+        day = '0' + day if len(day) < 2 else day
+        month = '0' + month if len(month) < 2 else month
+        if len(year) < 4:
+            if int(year) > 24:
+                year = '19' + year
+            else:
+                year = '20' + year
+        return f'{month}-{day}-{year}'
+    except:
+        return ''
+
+
 FINAL_ATHLETES_COLUMNS = ['athlete_id', 'firstName', 'lastName', 'displayHeight', 'displayWeight',
                           'displayDOB']
 df_athletes = pd.read_csv('data/athletes.csv')
 df_athletes_decomposed = df_athletes[FINAL_ATHLETES_COLUMNS]
 df_athletes_decomposed['weight'] = df_athletes_decomposed['displayWeight']\
     .apply(extract_number)
-
+df_athletes_decomposed['heightInches'] = df_athletes_decomposed['displayHeight'].apply(get_height_inches)
+df_athletes_decomposed['dob'] = df_athletes_decomposed['displayDOB'].apply(clean_date)
+df_athletes_decomposed.drop(columns=['displayHeight', 'displayWeight', 'displayDOB'], inplace=True)
+df_athletes_decomposed.to_csv('data/decomposed_data/athletes.csv')
+#%%
 #%%
