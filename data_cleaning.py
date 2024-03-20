da8cd7d4198dd9e88368f0a97a9e6b824432860a
cazumbaugh3 <czumbaugh@platinumdeveloping.net>
2024-03-20 08:27:58 -0500

    On second thought, since the team to games relation ship is one-to-many, we should keep the home and away team ids in the games table and do away with a separate table for the relationship.

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,32 +1,14 @@
-import numpy as np
 import pandas as pd
 import re
 
 #%%
 """
-We need to break the games file up and extract the home and away teams.
-The new table will have attributes game_id, team_id, and type.
+We need to remove some of the un-needed columns from games
 """
 FINAL_GAMES_COLUMNS = ['game_id', 'year', 'season_type', 'week', 'date', 'name', 'shortName', 'attendance',
-                       'venue_id', 'home_win_bool']
+                       'venue_id', 'home_win_bool', 'home_team_id', 'away_team_id']
 # Read in the games data
 df_games = pd.read_csv('data/games.csv')
-# Get home team data and set the type
-df_home_team = df_games[['game_id', 'home_team_id']]
-df_home_team = df_home_team.rename(columns={'home_team_id': 'team_id'})
-df_home_team['type'] = 'home'
-# Get away team data and set type
-df_away_team = df_games[['game_id', 'away_team_id']]
-df_away_team = df_away_team.rename(columns={'away_team_id': 'team_id'})
-df_away_team['type'] = 'away'
-
-# Stack the data and sort by the game ID
-df_team_games = pd.concat([df_home_team, df_away_team], axis=0).sort_values(by='game_id')
-
-# Sanity check that each game has only 2 teams
-df_team_games.groupby('game_id').agg({'team_id': len}).agg('max')
-
-df_team_games.to_csv('data/decomposed_data/team_games.csv', index=False)
 
 df_games_decomposed = df_games[FINAL_GAMES_COLUMNS]
 df_games_decomposed.to_csv('data/decomposed_data/decomposed_games.csv', index=False)
