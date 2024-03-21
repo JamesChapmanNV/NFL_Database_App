f28edb4b7696a73ddc4b3d5328386a79e5fa0c2a
cazumbaugh3 <czumbaugh@platinumdeveloping.net>
2024-03-20 21:52:07 -0500

    We have some FD issues here, but I don't think they are real, just the result of dirty data. For example, firstname, lastname, displaybirthplace -> displaydraft but not athlete_id. I think it is reasonable that there can be the same name for different players, and I think after cleaning the data these issues will resolve.

--- src/sql/Functional Dependencies/athletes_fd_check.sql
+++ src/sql/Functional Dependencies/athletes_fd_check.sql
@@ -41,3 +41,69 @@ FROM
 		FROM athletes
 		GROUP BY firstname, lastname
 		HAVING count(DISTINCT displaydraft) > 1) x;
+		
+-- firstname, lastname, displaybirthplace -> athlete_id does not hold
+SELECT CASE
+		WHEN count(firstname) = 0 THEN 'true'
+		ELSE 'false'
+		END AS holds
+FROM
+	(SELECT firstname
+		FROM athletes
+		GROUP BY firstname, lastname, displaybirthplace
+		HAVING count(DISTINCT athlete_id) > 1) x;
+		
+-- firstname, lastname, displaybirthplace -> displaydob holds
+SELECT CASE
+		WHEN count(firstname) = 0 THEN 'true'
+		ELSE 'false'
+		END AS holds
+FROM
+	(SELECT firstname
+		FROM athletes
+		GROUP BY firstname, lastname, displaybirthplace
+		HAVING count(DISTINCT displaydob) > 1) x;
+		
+-- firstname, lastname, displaybirthplace -> debutyear does not hold
+SELECT CASE
+		WHEN count(firstname) = 0 THEN 'true'
+		ELSE 'false'
+		END AS holds
+FROM
+	(SELECT firstname
+		FROM athletes
+		GROUP BY firstname, lastname, displaybirthplace
+		HAVING count(DISTINCT debutyear) > 1) x;
+		
+-- firstname, lastname, displaybirthplace -> displaydraft holds
+SELECT CASE
+		WHEN count(firstname) = 0 THEN 'true'
+		ELSE 'false'
+		END AS holds
+FROM
+	(SELECT firstname
+		FROM athletes
+		GROUP BY firstname, lastname, displaybirthplace
+		HAVING count(DISTINCT displaydraft) > 1) x;
+		
+-- firstname, lastname, displaybirthplace -> age does not hold
+SELECT CASE
+		WHEN count(firstname) = 0 THEN 'true'
+		ELSE 'false'
+		END AS holds
+FROM
+	(SELECT firstname
+		FROM athletes
+		GROUP BY firstname, lastname, displaybirthplace
+		HAVING count(DISTINCT age) > 1) x;
+
+-- firstname, lastname, displaydob -> athlete_id does not hold
+SELECT CASE
+		WHEN count(firstname) = 0 THEN 'true'
+		ELSE 'false'
+		END AS holds
+FROM
+	(SELECT firstname
+		FROM athletes
+		GROUP BY firstname, lastname, displaydob
+		HAVING count(DISTINCT athlete_id) > 1) x;a
