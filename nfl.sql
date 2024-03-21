c19b58a9d6e439e6e558854f61f3ddf696f291ed
James Chapman <89288003+JamesChapmanNV@users.noreply.github.com>
2024-03-20 23:31:18 -0500

    New notebook and NFLSQL

--- nfl.sql
+++ nfl.sql
@@ -5,29 +5,41 @@
 -- is in the data directory.  
 
 DROP TABLE games;
-
 CREATE TABLE games (
 	game_id INT,
-	year INT,
-	season_type INT,
-	week INT,
-	date varchar(20),
 	name varchar(50),
 	shortName varchar(10),
+	date varchar(20),
 	attendance INT,
 	venue_id INT,
 	home_team_id INT,
-	home_score INT,
 	away_team_id INT,
-	away_score INT,
-	home_win_bool BOOLEAN,
-	away_win_bool BOOLEAN 
+	utc_time varchar(20)
 );
-
+-- home_score INT,
+-- away_score INT,
+-- home_win_bool BOOLEAN
 \copy games from 'data/games.csv' DELIMITER ',' CSV HEADER;
 
-DROP TABLE rosters;
+DROP TABLE season_dates;
+CREATE TABLE season_dates (
+	date varchar(30),
+	season_year INT,
+	season_type varchar(30),
+	week INT
+);
+\copy season_dates from 'data/season_dates.csv' DELIMITER ',' CSV HEADER;
 
+DROP TABLE linescores;
+CREATE TABLE linescores (
+	game_id INT,
+	team_id INT,
+	quarter INT,
+	score INT
+);
+\copy linescores from 'data/linescores.csv' DELIMITER ',' CSV HEADER;
+
+DROP TABLE rosters;
 CREATE TABLE rosters (
 	game_id INT,
 	team_id INT,
@@ -36,70 +48,54 @@ CREATE TABLE rosters (
 	active BOOLEAN,
 	didNotPlay BOOLEAN
 );
-
 \copy rosters from 'data/rosters.csv' DELIMITER ',' CSV HEADER;
 
 DROP TABLE athletes;
-
 CREATE TABLE athletes (
 	athlete_id INT,
-	firstName varchar(20),
-	lastName varchar(20),
-	displayName varchar(30),
-	fullName varchar(30),
-	debutYear INT,
-	displayBirthPlace varchar(30),
-	displayHeight varchar(10),
-	displayWeight varchar(10),
-	displayDOB varchar(10),
-	age INT,
-	displayDraft varchar(30)
+	firstName varchar(30),
+	lastName varchar(30),
+	birth_place varchar(30),
+	drafted_bool varchar(30),
+	jersey INT,
+	heightInches INT,
+	weight INT,
+	dob varchar(30)
 );
-
+-- drafted_bool BOOLEAN
 \copy athletes from 'data/athletes.csv' DELIMITER ',' CSV HEADER;
 
 DROP TABLE venues;
-
 CREATE TABLE venues (
 	venue_id INT,
 	fullName varchar(40),
-	capacity INT,
 	grass BOOLEAN,
 	indoor BOOLEAN,
 	city varchar(20),
 	state varchar(10),
 	zipCode INT
 );
-
+-- capacity INT,
 \copy venues from 'data/venues.csv' DELIMITER ',' CSV HEADER;
 
 DROP TABLE teams;
-
 CREATE TABLE teams (
 	team_id INT,
-	slug varchar(30),
 	location varchar(20),
-	name varchar(10),
-	nickname varchar(10),
-	abbreviation varchar(10),
-	displayName varchar(30),
-	shortDisplayName varchar(10),
-	color varchar(10),
-	alternateColor varchar(10),
-	franchise_id INT
+	name varchar(20),
+	abbreviation varchar(20),
+	venue_id INT,
+	primary_color varchar(20),
+	secondary_color varchar(20)
 );
-
 \copy teams from 'data/teams.csv' DELIMITER ',' CSV HEADER;
 
 DROP TABLE positions;
-
 CREATE TABLE positions (
-	id INT,
+	position_id INT,
 	name varchar(20),
-	displayName varchar(20),
 	abbreviation varchar(10)
 );
-
 \copy positions from 'data/positions.csv' DELIMITER ',' CSV HEADER;
 
 
