78e9a9b45b7b69581b0f07dce21251153b8551ac
cazumbaugh3 <czumbaugh@platinumdeveloping.net>
2024-04-16 19:55:45 -0500

    Added query to retrieve venues and team name.

---
+++ src/sql/Sample Queries/Venue.sql
@@ -0,0 +1,7 @@
+-- Get the venue information, including the home team that matches the given
+-- search criteria.
+-- This is a question type query
+SELECT v.*, teams.team_name
+FROM venues v
+JOIN teams ON teams.venue_name = v.venue_name
+WHERE v.venue_name LIKE '%';
