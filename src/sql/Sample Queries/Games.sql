2bea34bee8a057041fd6af52524c8d12e0162dca
cazumbaugh3 <czumbaugh@platinumdeveloping.net>
2024-04-16 20:36:55 -0500

    Added query to pull games and compute the final score.
    The winner is determined from the final score, and the primary and secondary colors returned are the colors of the winning team.

---
+++ src/sql/Sample Queries/Games.sql
@@ -0,0 +1,25 @@
+-- Find information for the requested game, including the scores and winner.
+-- Question query
+SELECT g.*,
+       home_score.final_score_home,
+       away_score.final_score_away,
+       CASE
+           WHEN home_score.final_score_home > away_score.final_score_away THEN home.primary_color
+           ELSE away.primary_color END   AS primary_color,
+       CASE
+           WHEN home_score.final_score_home > away_score.final_score_away THEN home.secondary_color
+           ELSE away.secondary_color END AS secondary_color
+FROM games g
+         JOIN (SELECT game_id, team_name, SUM(score) AS final_score_home
+               FROM linescores
+               GROUP BY game_id, team_name) home_score
+              ON home_score.game_id = g.game_id AND home_score.team_name = g.home_team_name
+         JOIN (SELECT game_id, team_name, SUM(score) AS final_score_away
+               FROM linescores
+               GROUP BY game_id, team_name) away_score
+              ON away_score.game_id = g.game_id AND away_score.team_name = g.away_team_name
+         JOIN teams home ON home.team_name = g.home_team_name
+         JOIN teams away ON away.team_name = g.away_team_name
+WHERE g.game_id = 330106033
+  AND home_score.team_name = g.home_team_name
+  AND away_score.team_name = g.away_team_name;
