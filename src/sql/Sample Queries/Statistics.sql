31ed56881e33c6918fa3f75aeb0010bf4273508b
cazumbaugh3 <czumbaugh@platinumdeveloping.net>
2024-04-18 17:02:46 -0500

    Updated query for new rosters table design

--- src/sql/Sample Queries/Statistics.sql
+++ src/sql/Sample Queries/Statistics.sql
@@ -6,15 +6,15 @@
 -- Find total passing, rushing, and receiving yards for top players in a given game
 WITH stats AS (SELECT first_name, last_name, position_name, r.team_name, play_type, total_yards
                FROM athletes ath
-                        JOIN (SELECT play_type, athlete_id, SUM(yards) AS total_yards
+                        JOIN (SELECT play_type, athlete_id, date, SUM(yards) AS total_yards
                               FROM games
                                        JOIN player_plays pp ON games.game_id = pp.game_id
                                        JOIN plays p ON pp.play_id = p.play_id
                                        JOIN athletes a ON a.athlete_id = pp.player_id
                               WHERE games.game_id = 401437790
                                 AND play_type IN ('Rush', 'Pass Reception')
-                              GROUP BY play_type, a.athlete_id) AS x ON x.athlete_id = ath.athlete_id
-                        JOIN rosters r ON r.athlete_id = ath.athlete_id AND r.game_id = 401437790
+                              GROUP BY play_type, a.athlete_id, date) AS x ON x.athlete_id = ath.athlete_id
+                        JOIN rosters r ON r.athlete_id = ath.athlete_id AND r.start_date <= x.date AND r.end_data >= x.date
                ORDER BY x.play_type, total_yards DESC)
 SELECT *
 FROM stats
