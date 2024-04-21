33b3bada6b1bcba45f8bb7f4f415726fb0663a82
vishnuteja97 <vteja.bon997@gmail.com>
2024-04-21 12:32:53 -0500

    Update Passing.sql
    Added query result for year 2015 as comment

--- src/sql/Sample Queries/Passing.sql
+++ src/sql/Sample Queries/Passing.sql
@@ -73,3 +73,61 @@ JOIN
     athletes a ON stats.player_id = a.athlete_id -- Joining athletes table to access player names.
 ORDER BY 
     passing_yards DESC; -- Ordering the results by passing yards in descending order.
+
+/*(Partial) Query result
+ player_id | first_name |   last_name    | passing_yards | pass_attempts | pass_completions | touchdown_passes | passes_intercepted | passer_rating
+-----------+------------+----------------+---------------+---------------+------------------+------------------+--------------------+---------------
+      2330 | Tom        | Brady          |          5602 |           752 |              491 |               46 |                 10 |          56.3
+      2580 | Drew       | Brees          |          4646 |           586 |              396 |               31 |                 10 |          56.3
+     14881 | Russell    | Wilson         |          4612 |           524 |              348 |               37 |                 13 |          60.4
+      5529 | Philip     | Rivers         |          4606 |           621 |              415 |               27 |                  7 |          56.3
+      4459 | Carson     | Palmer         |          4554 |           520 |              329 |               34 |                 10 |          60.4
+     12483 | Matthew    | Stafford       |          4445 |           594 |              397 |               30 |                 14 |          56.3
+     16724 | Blake      | Bortles        |          4368 |           575 |              341 |               35 |                 13 |          56.3
+      5526 | Eli        | Manning        |          4315 |           575 |              362 |               33 |                 12 |          56.3
+     11237 | Matt       | Ryan           |          4269 |           581 |              384 |               19 |                 14 |          56.3
+     13994 | Cam        | Newton         |          4175 |           540 |              318 |               37 |                 12 |          56.3
+     14880 | Kirk       | Cousins        |          4109 |           534 |              368 |               26 |                 10 |          56.3
+     16757 | Derek      | Carr           |          4042 |           532 |              326 |               31 |                  9 |          56.3
+      8439 | Aaron      | Rodgers        |          4041 |           602 |              360 |               34 |                 10 |          52.1
+      5536 | Ben        | Roethlisberger |          3987 |           475 |              323 |               19 |                 16 |          60.4
+     14876 | Ryan       | Tannehill      |          3968 |           544 |              337 |               22 |                 10 |          56.3
+   2969939 | Jameis     | Winston        |          3807 |           486 |              280 |               22 |                 11 |          56.3
+      8664 | Ryan       | Fitzpatrick    |          3741 |           521 |              315 |               29 |                 12 |          56.3
+      9597 | Jay        | Cutler         |          3502 |           460 |              295 |               19 |                  5 |          56.3
+     13197 | Sam        | Bradford       |          3479 |           494 |              315 |               17 |                 12 |          56.3
+     11252 | Joe        | Flacco         |          3426 |           485 |              311 |               20 |                 13 |          56.3
+     14012 | Andy       | Dalton         |          3424 |           418 |              270 |               25 |                  7 |          60.4
+      8416 | Alex       | Smith          |          3416 |           441 |              287 |               18 |                  5 |          56.3
+     16728 | Teddy      | Bridgewater    |          3203 |           427 |              280 |               14 |                  8 |          56.3
+     14874 | Andrew     | Luck           |          2885 |           421 |              236 |               18 |                 16 |          52.1
+   2576980 | Marcus     | Mariota        |          2866 |           368 |              229 |               19 |                  8 |          56.3
+     14163 | Tyrod      | Taylor         |          2799 |           353 |              224 |               20 |                  6 |          56.3
+      1428 | Peyton     | Manning        |          2498 |           366 |              219 |               10 |                 14 |          52.1
+     12477 | Brian      | Hoyer          |          2368 |           332 |              199 |               18 |                  5 |          56.3
+      3609 | Josh       | McCown         |          2130 |           291 |              185 |               12 |                  3 |          56.3
+     14877 | Nick       | Foles          |          2066 |           333 |              188 |                7 |                  8 |          52.1
+      1575 | Matt       | Hasselbeck     |          1799 |           259 |              157 |                9 |                  5 |          52.1
+     14879 | Brock      | Osweiler       |          1788 |           251 |              154 |                9 |                  4 |          56.3
+     13987 | Blaine     | Gabbert        |          1730 |           240 |              151 |                9 |                  6 |          56.3
+     14001 | Colin      | Kaepernick     |          1628 |           241 |              143 |                6 |                  3 |          52.1
+     16736 | Johnny     | Manziel        |          1597 |           221 |              127 |                7 |                  5 |          56.3
+      5209 | Tony       | Romo           |          1433 |           168 |              116 |                9 |                  5 |          60.4
+      8644 | Matt       | Cassel         |          1296 |           204 |              119 |                5 |                  5 |          52.1
+     14878 | Brandon    | Weeden         |          1126 |           141 |               96 |                5 |                  2 |          56.3
+     14037 | Ryan       | Mallett        |          1078 |           188 |              105 |                4 |                  4 |          47.9
+     16814 | Zach       | Mettenberger   |           998 |           154 |               97 |                4 |                  6 |          52.1
+     13198 | Jimmy      | Clausen        |           747 |           124 |               72 |                2 |                  3 |          52.1
+     16810 | AJ         | McCarron       |           731 |            91 |               62 |                4 |                  1 |          60.4
+     15168 | Case       | Keenum         |           646 |            89 |               54 |                4 |                  1 |          56.3
+     12482 | Mark       | Sanchez        |           640 |            91 |               60 |                4 |                  3 |          56.3
+     15904 | Landry     | Jones          |           600 |            55 |               32 |                3 |                  4 |          68.8
+     15803 | EJ         | Manuel         |           570 |            82 |               51 |                3 |                  2 |          52.1
+      5615 | Matt       | Schaub         |           534 |            78 |               52 |                3 |                  2 |          52.1
+     14882 | Kellen     | Moore          |           443 |            56 |               28 |                1 |                  4 |          56.3
+     14114 | T.J.       | Yates          |           391 |            57 |               28 |                3 |                  1 |          52.1
+      2549 | Mike       | Vick           |           391 |            68 |               40 |                2 |                  2 |          47.9
+     15187 | Austin     | Davis          |           371 |            48 |               32 |                1 |                  1 |          56.3
+      5631 | Luke       | McCown         |           327 |            38 |               31 |                0 |                  1 |          60.4
+:
+*/
