10c747098c84775381996827d25d7f5ca0c90798
cazumbaugh3 <czumbaugh@platinumdeveloping.net>
2024-04-29 20:07:18 -0500

    Added order by to display the results in descending order by average total points

--- src/sql/Sample Queries/AvgPtsGrassIndoor.sql
+++ src/sql/Sample Queries/AvgPtsGrassIndoor.sql
@@ -4,20 +4,21 @@
 -- grass/turf seems to not matter
 -- QUERY TYPE: report 
 
-SELECT 
-    CASE 
-        WHEN v.grass THEN 'Grass' 
-        ELSE 'Turf' 
+SELECT
+    CASE
+        WHEN v.grass THEN 'Grass'
+        ELSE 'Turf'
     END AS field,
-    CASE 
-        WHEN v.indoor THEN 'Indoor' 
-        ELSE 'Outdoor' 
+    CASE
+        WHEN v.indoor THEN 'Indoor'
+        ELSE 'Outdoor'
 	END AS venue,
 	AVG(gs.home_team_score + gs.away_team_score) AS avg_total_points
 FROM game_scores gs
 JOIN games g ON gs.game_id = g.game_id
 JOIN venues v ON g.venue_name = v.venue_name
-GROUP BY v.grass,v.indoor;
+GROUP BY v.grass,v.indoor
+ORDER BY avg_total_points DESC;
 
 /* SAMPLE OUTPUT
 
