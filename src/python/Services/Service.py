9f9a2615420fdae7ae02d3bb32b90af761987f06
cazumbaugh3 <czumbaugh@platinumdeveloping.net>
2024-05-02 17:25:01 -0500

    Added docstring to Service base class

--- src/python/Services/Service.py
+++ src/python/Services/Service.py
@@ -3,6 +3,12 @@ from abc import ABC, abstractmethod
 
 
 class Service(ABC):
+    """
+    Abstract base class that all services inherit from. This class contains a public method called set_connection
+    which allows the connection to be set. An abstract method called get_data is defined, which must be implemented
+    by the subclass. This is the primary public method to retrieve the desired data. It is up to the subclass to define
+    how the request is fulfilled.
+    """
     def __init__(self, conn: Connection = None):
         self.conn = conn
 
